d43c1c8bbb60e19e4493dd80f4fccc95eb15cde3
Amandine Audras <44638568+renardpal@users.noreply.github.com>
2026-08-19 15:29:48 +0200

    fix css?

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,31 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-
+import { Outlet, createRootRoute, HeadContent, Scripts } from '@tanstack/react-router'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import appCss from '@/styles.css?url'
 
 export const Route = createRootRoute({
+  head: () => ({
+    links: [{ rel: 'stylesheet', href: appCss }],
+  }),
   component: RootLayout,
 })
 
 function RootLayout() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <html lang="fr">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <div className="flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+        <Scripts />
+      </body>
+    </html>
   )
 }
